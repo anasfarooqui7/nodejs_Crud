cc6a6b7ea543154efa8a153bc639875ece350c2c
anasfarooqui7 <67817524+anasfarooqui7@users.noreply.github.com>
2025-04-04 02:12:39 +0530

    call middleware file

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const itemController = require("../controllers/itemControllers");
+const verifyAge = require("../middlewares/ageMiddleware"); // Import middleware
 
 // Routes
 router.get("/", itemController.getAllItems);
-router.post("/add", itemController.createItem);
-router.post("/update/:id", itemController.updateItem);
+router.post("/add", verifyAge, itemController.createItem); // Apply middleware
+router.post("/update/:id", verifyAge, itemController.updateItem); // Apply middleware
 router.get("/delete/:id", itemController.deleteItem);
 
 module.exports = router;
